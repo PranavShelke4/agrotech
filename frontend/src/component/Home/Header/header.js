a23e9bddc793f83b768132642751df7f4372ac48
Pranav <pranavshelke4@gmail.com>
2022-07-06 21:33:27 +0530

    header done

--- frontend/src/component/Home/Header/header.js
+++ frontend/src/component/Home/Header/header.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import "./header.css"
-import { Container } from 'react-bootstrap';
+import React from "react";
+import "./header.css";
+import { Container, Button } from "react-bootstrap";
 
 function header() {
   return (
-    <div className='header'>
-      <Container> 
-       <h1>  Smart way of
-Farming with Alagi </h1>
+    <div className="header">
+      <Container>
+        <h1 className="title">
+          <b>
+            {" "}
+            Smart way of <br></br> Farming with <br></br> Rutuja Krishi Seva
+            Kendra{" "}
+          </b>
+        </h1> 
+        <Button id="seeNow" variant="outline-light">See Now</Button>
+
       </Container>
     </div>
-  )
+  );
 }
 
-export default header
+export default header;
