0bee6d18d839da41508905e31c9819e80c06cab7
Pranav <pranavshelke4@gmail.com>
2022-07-06 11:19:31 +0530

    some file add

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../component/header'
+import Footer from '../component/footer'
+
+function Home() {
+  return (
+    <>
+        <Header />
+        <Footer />
+    </>
+  )
+}
+
+export default Home
