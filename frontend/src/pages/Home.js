d3567ee41021c2e60a42a9936599a8fde93b0444
Aryan-Jagadale <aryansjagadale@gmail.com>
2022-07-06 12:35:34 +0530

    Brands done

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../component/Header/header'
 import Footer from '../component/Footer/footer'
+import Brands from '../component/Brands/brands'
 
 function Home() {
   return (
     <>
         <Header />
+        <Brands/>
         <Footer />
     </>
   )
