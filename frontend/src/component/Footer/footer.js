eba5c07636f335e6c475254575a62488e6d0b463
Aryan-Jagadale <aryansjagadale@gmail.com>
2022-07-06 11:43:58 +0530

    Footer started working

--- frontend/src/component/Footer/footer.js
+++ frontend/src/component/Footer/footer.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import './footer.scss'
+
 
 function footer() {
   return (
-    <div>footer</div>
+    <>
+      <div className='footer-container'>
+        <div className='footer-logosection'>
+          
+        </div>
+        
+      </div>
+    
+    
+    </>
   )
 }
 
