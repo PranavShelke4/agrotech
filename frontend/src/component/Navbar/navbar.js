5365d87e5ce676113ed078bf2fac6ad614163eae
Pranav <pranavshelke4@gmail.com>
2022-07-06 18:28:23 +0530

    navbar done

---
+++ frontend/src/component/Navbar/navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./navbar.css"
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+
+function navbar() {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="../../assets/images/logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            React-Bootstrap
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link id= "nav1" href="#home">Home</Nav.Link>
+              <Nav.Link id="navLink" href="#link">Products</Nav.Link>
+              <Nav.Link id="navLink" href="#link" disabled>
+                Visitors
+              </Nav.Link>
+              <Nav.Link id="navLink" href="#link">Contect us</Nav.Link>
+              <Button id="navLink" variant="outline-light">Login</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default navbar;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from '../component/Navbar/navbar'
 import Header from '../component/Header/header'
 import Footer from '../component/Footer/footer'
 
 function Home() {
   return (
     <>
+        <Navbar />
         <Header />
         <Footer />
     </>
